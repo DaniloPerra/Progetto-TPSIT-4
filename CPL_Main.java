d857c2b13082db53c49be9a4c5737644a9020700
marco-cecili <marco8cecili@gmail.com>
2026-03-23 18:12:39 +0100

    Creazione di un utente all'interno del main e mostrare a schermo le informazioni dell'utente
    svolto "issue 7"

---
+++ CPL_Main.java
@@ -0,0 +1,22 @@
+package ceciliJava;
+
+import java.util.*;
+
+public class CPL_Main {
+
+	public static void main(String[] args) {
+		
+		Scanner tastiera = new Scanner(System.in);
+		
+		Utente utente = new Utente();
+		
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("$$$$	  Benvenuti nell'applicazione di giochi d'azzardo migliore al mondo  	    $$$$");
+		System.out.println("$$$$$$  		Dove il gioco e' responsabile e sicuro			  $$$$$$");
+		System.out.println("$$$$$$$$			Benvenuti in CPL Gambling			$$$$$$$$");
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("$$$$$$$$			   Registrazione				$$$$$$$$");
+		utente.inserisciDati(tastiera);
+		System.out.print(utente.toString());
+
+}
