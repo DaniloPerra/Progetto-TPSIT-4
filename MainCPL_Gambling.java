ba534f073c3a63743a13ea5b8b8cd4021a1d88de
DaniloPerra <perradanilo8@gmail.com>
2026-03-24 21:10:36 +0100

    Classe principale e Main classe principale
    Carico la classe principale del proggetto ed il main con un vettore di oggetti della classe principale. Ho riscontrato dei problemi con un attributo in particolare, ho tentato di crearlo di tipo arraylist ma mi da problemi nell'inserisciDati, tenterò di risolvere il problema.

---
+++ MainCPL_Gambling.java
@@ -0,0 +1,24 @@
+package ProggettoInformaticaTpsit;
+import java.util.*;
+
+public class MainCPL_Gambling {
+    public static void main(String args[]) {
+
+        Scanner tastiera = new Scanner(System.in);
+
+        System.out.println("Quanti sedi ha il CPL Gambling?");
+        int n = tastiera.nextInt();
+
+        CPL_Gambling cplMomentaneo = new CPL_Gambling();
+        CPL_Gambling[] arrayCPL = new CPL_Gambling[n];
+
+        for (int i = 0; i < n; i++) {
+            cplMomentaneo.inserisciDati(tastiera);
+            arrayCPL[i] = cplMomentaneo;
+        }
+
+        for (int j = 0; j < n; j++) {
+            System.out.println(arrayCPL[j].toString());
+        }
+    }
+}
